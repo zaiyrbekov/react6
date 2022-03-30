7a1dec2d8937883e727f0f68b3ea28aa562e71d1
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-03-30 10:51:14 +0600

    comented files

--- src/components/ArticleList/ArtcleList.js
+++ src/components/ArticleList/ArtcleList.js
@@ -1,12 +1,12 @@
-function ArticleList(props) {
-  return props.src.map((article) => (
-    <article>
-      <h3>
-        <a href={article.url}>{article.title}</a>
-      </h3>
-      <p>{article.teaser}</p>
-    </article>
-  ));
-}
+// function ArticleList(props) {
+//   return props.src.map((article) => (
+//     <article>
+//       <h3>
+//         <a href={article.url}>{article.title}</a>
+//       </h3>
+//       <p>{article.teaser}</p>
+//     </article>
+//   ));
+// }
 
-export default ArticleList;
+// export default ArticleList;
